b5f0872125d439012a06b4a4cc39668f4565bbff
bssughosh <bssughosh27@gmail.com>
2020-05-18 20:07:09 +0530

    hidden API key

--- weather/fetch_weather_data.py
+++ weather/fetch_weather_data.py
@@ -6,11 +6,15 @@ Author: Sughosh Sudhanvan
 """
 
 from wwo_hist import retrieve_hist_data
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 frequency = 24
 start_date = '1-JAN-2020'
 end_date = '1-MAR-2020'
-api_key = 'f825978565ab431e8de135522201404'
+api_key = os.getenv("SECRET_KEY")
 location_list = ['mumbai']
 hist_weather_data = retrieve_hist_data(api_key,
                                        location_list,
