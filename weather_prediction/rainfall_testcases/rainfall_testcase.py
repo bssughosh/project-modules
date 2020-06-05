e22ed97258114a22db308b1021dbafdf9ef043cf
bssughosh <bssughosh27@gmail.com>
2020-06-05 14:24:13 +0530

    Rainfall Prediction

--- weather_prediction/rainfall_testcases/rainfall_testcase.py
+++ weather_prediction/rainfall_testcases/rainfall_testcase.py
@@ -44,13 +44,20 @@ df3 = df2[['Year', selected_month]]
 df3 = pd.DataFrame(df3, columns=['Year', selected_month])
 
 x = []
+t = []
 for i, j in df3.iterrows():
+    # if j[0] == '2008':
+    #     t.append(j)
+    # elif j[0] == '2010' or j[0] == '2009':
+    #     pass
     x.append(j)
 
+del x[0]
 x1 = pd.DataFrame(x, columns=['Year', selected_month])
+# x2 = pd.DataFrame(t, columns=['Year',selected_month])
 
 forecast_out = 10
-x1['prediction'] = x1[['Jul']].shift(-forecast_out)
+x1['prediction'] = x1[[selected_month]].shift(-forecast_out)
 
 X = np.array(x1.drop(['Year', 'prediction'], 1))
 X = X[:-forecast_out]
@@ -63,3 +70,45 @@ svm.fit(X, y)
 
 x_forecast = np.array(x1.drop(['Year', 'prediction'], 1))[-forecast_out:]
 svm_prediction = svm.predict(x_forecast)
+
+# i = 300
+# mini = [np.inf, np.inf, 0]
+# ind = i
+# while i<=3000:
+#     svm = SVR(kernel='rbf', C=i)
+#     svm.fit(X, y)
+
+#     x_forecast = np.array(x1.drop(['Year','prediction'], 1))[-forecast_out:]
+#     svm_prediction = svm.predict(x_forecast)
+
+#     svm_prediction = pd.DataFrame(svm_prediction, columns=['Predicted'])
+#     x2.index = svm_prediction.index
+
+#     svm_prediction['Original'] = x2[selected_month]
+#     svm_prediction['diff'] = abs(svm_prediction['Predicted'] - svm_prediction['Original'])
+
+#     average_error = svm_prediction['diff'].sum()
+#     max_difference = svm_prediction['diff'].max()
+#     average_error = average_error / forecast_out
+#     if average_error < mini[0]:
+#         mini[0] = average_error
+#         mini[1] = max_difference
+#         mini[2] = svm_prediction.iloc[0,0]
+#         ind = i
+#     elif average_error == mini[0] and max_difference < mini[1]:
+#         mini[0] = average_error
+#         mini[1] = max_difference
+#         mini[2] = svm_prediction.iloc[0,0]
+#         ind = i
+
+#     i += 1
+
+
+# g = df.groupby(['State', 'District', 'Year'], as_index=False)
+
+# x = []
+# y = np.array(g)
+# for i,j in g:
+#     x.append(np.array(j))
+
+# x = np.array(x)
